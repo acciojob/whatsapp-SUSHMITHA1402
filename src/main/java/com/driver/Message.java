2243841b806db6b5aea7008fe6afafeb1050e4c7
SUSHMITHA1402 <gummadis999@gmail.com>
2023-01-28 17:14:53 +0530

    initial commit

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -3,8 +3,28 @@ package com.driver;
 import java.util.Date;
 
 public class Message {
-    private int id;
+    private static int id=0;
     private String content;
     private Date timestamp;
+
+    public Message(){}
+    public Message(String content){
+        id = id+1;
+        this.content = content;
+        this.timestamp = new Date();
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public Date getTimestamp(){
+        return timestamp;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
 
 }
--- src/main/java/com/driver/WhatsappController.java
+++ src/main/java/com/driver/WhatsappController.java
@@ -23,11 +23,10 @@ public class WhatsappController {
     WhatsappService whatsappService = new WhatsappService();
 
     @PostMapping("/add-user")
-    public String createUser(String name, String mobile) throws Exception {
+    public String createUser(String name,String mobile) throws Exception {
         //If the mobile number exists in database, throw "User already exists" exception
         //Otherwise, create the user and return "SUCCESS"
-
-        return whatsappService.createUser(name, mobile);
+            return whatsappService.createUser(name, mobile);
     }
 
     @PostMapping("/add-group")
@@ -58,7 +57,12 @@ public class WhatsappController {
         //Throw "You are not allowed to send message" if the sender is not a member of the group
         //If the message is sent successfully, return the final number of messages in that group.
 
-        return whatsappService.sendMessage(message, sender, group);
+        try {
+            return whatsappService.sendMessage(message, sender, group);
+        }
+        catch (Exception e){
+            throw new Exception();
+        }
     }
     @PutMapping("/change-admin")
     public String changeAdmin(User approver, User user, Group group) throws Exception{
@@ -70,7 +74,7 @@ public class WhatsappController {
         return whatsappService.changeAdmin(approver, user, group);
     }
 
-    @DeleteMapping("/remove-user")
+    /* @DeleteMapping("/remove-user")
     public int removeUser(User user) throws Exception{
         //This is a bonus problem and does not contains any marks
         //A user belongs to exactly one group
@@ -89,5 +93,7 @@ public class WhatsappController {
         // If the number of messages between given time is less than K, throw "K is greater than the number of messages" exception
 
         return whatsappService.findMessage(start, end, K);
-    }
+    } /*
+
+     */
 }
